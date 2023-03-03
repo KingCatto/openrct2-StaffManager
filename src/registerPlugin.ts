66d5c487981fd79ce04f347aa964c550df2ba17e
Dallas-Powers <sallad2012@gmail.com>
2023-03-03 08:54:44 -0500

    Update Plugin information.

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -3,11 +3,11 @@
 import main from './main';
 
 registerPlugin({
-  name: 'Dallas Hirestaff Test',
-  version: '0.0.1', // Your plugin version
-  authors: ['DallasP'],
+  name: 'Staff Manager',
+  version: '0.0.2', // Your plugin version
+  authors: ['Dallas Powers'],
   type: 'local',
-  licence: "GPL-3.0",
+  licence: "MIT",
   minApiVersion: 56,
   targetApiVersion: 56,
   main,
