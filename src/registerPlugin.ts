f7feb68b77d1fd5a83c45ce839f701e14e3710da
Dallas-Powers <sallad2012@gmail.com>
2023-03-03 10:47:44 -0500

    Change of username.

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -5,7 +5,7 @@ import main from './main';
 registerPlugin({
   name: 'Staff Manager',
   version: '0.0.3', // Your plugin version
-  authors: ['DallasP'],
+  authors: ['KingCatto'],
   type: 'local',
   licence: "MIT",
   minApiVersion: 56,
